a3ad451ece00759cfc7f9e26006780ae3c094b78
Harrison Ng <tplawyer@gmail.com>
2015-03-02 23:17:26 +0800

    New Kalman Filter

--- inc/kalman.h
+++ inc/kalman.h
@@ -1,3 +1,4 @@
+/*
 typedef struct kalman_filter KF;
 
 #ifndef __KALMAN_H
@@ -13,3 +14,37 @@ void kalman_filter_init(KF *kf, float Q, float* R, float X, float P);
 void kalman_filtering(KF *kf, float* output, float *data1, float *data2, int length);
 
 #endif
+ */
+/*
+ * Kalman.h
+ *
+ *  Created on: 2014¦~8¤ë24¤é
+ *      Author: YunKei
+ */
+
+#ifndef KALMAN_H_
+#define KALMAN_H_
+
+
+class Kalman{
+
+public:
+
+	Kalman(double, double*, double, double);
+	void Filtering(double*, double, double);
+
+private:
+
+	double _Q, _R[2],__X, _x,__P, _p, _K[2], _yk[2], _Sk[2][2];
+	bool isOneDim;
+	void MeasurementResidual(double, double);
+	void MeasurementResidualCovariance();
+	void StatePredict();
+	void CovariancePredict();
+	void StateUpdate();
+	void CovarianceUpdate();
+	void Gain();
+};
+
+
+#endif /* KALMAN_H_ */
--- src/kalman.cpp
+++ src/kalman.cpp
@@ -1,4 +1,6 @@
+/*
 #include <kalman.h>
+#include <cstdlib>
 
 void kalman_filter_init(KF *kf, float Q, float* R, float X, float P){
 	kf->Q = Q;
@@ -47,7 +49,7 @@ void kalman_covariance_update(KF *kf){
 
 void kalman_gain(KF *kf){
 	float inv_Sk[2][2];
-	float det = 1 / (kf->Sk[0][0]*kf->Sk[1][1] - kf->Sk[0][1]*kf->Sk[1][0]);
+	float det = 1.0f / (kf->Sk[0][0]*kf->Sk[1][1] - kf->Sk[0][1]*kf->Sk[1][0]);
 	inv_Sk[0][0] = det * kf->Sk[1][1];
 	inv_Sk[0][1] = -det * kf->Sk[0][1];
 	inv_Sk[1][0] = -det * kf->Sk[1][0];
@@ -57,7 +59,11 @@ void kalman_gain(KF *kf){
 }
 
 void kalman_filtering(KF *kf, float* output, float* data1, float* data2, int length){
+
 	for(int i = 0; i < length; i++){
+		if(fabs(kf[i].X - *data1) < 0.000001f || fabs(kf[i].X - *data2) < 0.000001f){
+			return;
+		}
 		kalman_state_predict(&kf[i]);
 		kalman_covariance_predict(&kf[i]);
 		kalman_measurement_residual(&kf[i], data1[i], data2[i]);
@@ -68,3 +74,100 @@ void kalman_filtering(KF *kf, float* output, float* data1, float* data2, int len
 		output[i] = kf[i].X;
 	}
 }
+*/
+/*
+ * Kalman.cpp
+ *
+ *  Created on: 2014Š~8€ë24€é
+ *      Author: YunKei
+ */
+
+#include <kalman.h>
+#include <stdio.h>
+
+Kalman::Kalman(double Q, double* R, double X, double P) : _Q(0), __X(0), _x(0), __P(0), _p(0), isOneDim(false){
+	_Q = Q;
+	_R[0] = R[0];
+	_R[1] = R[1];
+	_K[0] = 0;
+	_K[1] = 0;
+	__X = X;
+	_x = 0;
+	__P = P;
+	_p = 0;
+	_yk[0] = 0;
+	_yk[1] = 0;
+	_Sk[0][0] = 0;
+	_Sk[0][1] = 0;
+	_Sk[1][0] = 0;
+	_Sk[1][1] = 0;
+	isOneDim = _R[1] < 0 ? true : false;
+}
+
+void Kalman::Filtering(double* output, double data1, double data2){
+
+	StatePredict();
+	CovariancePredict();
+	MeasurementResidual(data1, data2);
+	MeasurementResidualCovariance();
+	Gain();
+	StateUpdate();
+	CovarianceUpdate();
+	*output = __X;
+}
+
+void Kalman::MeasurementResidual(double Z1, double Z2){
+	_yk[0] = Z1 - _x;
+	if(!isOneDim){
+		_yk[1] = Z2 - _x;
+	}
+}
+void Kalman::MeasurementResidualCovariance(){
+	_Sk[0][0] = _p + _R[0];
+	if(!isOneDim){
+		_Sk[0][1] = _Sk[1][0] = _p;
+		_Sk[1][1] = _p + _R[1];
+	}
+}
+void Kalman::StatePredict(){
+	_x = __X;
+}
+void Kalman::CovariancePredict(){
+	_p = __P + _Q;
+}
+void Kalman::StateUpdate(){
+
+	if(!isOneDim){
+		__X = _x + _K[0] * _yk[0] + _K[1] * _yk[1];
+	}
+	else{
+		__X = _x + _K[0] * _yk[0];
+	}
+}
+void Kalman::CovarianceUpdate(){
+
+	if(!isOneDim){
+		__P = (1 - _K[0] + _K[1]) * _p;
+	}
+	else{
+		__P = (1 - _K[0]) * _p;
+	}
+}
+void Kalman::Gain(){
+	if(!isOneDim){
+
+		double inv_Sk[2][2];
+		double inv_det = _Sk[0][0] *_Sk[1][1] - _Sk[0][1] *_Sk[1][0];
+		inv_Sk[0][0] = _Sk[1][1] / inv_det;
+		inv_Sk[0][1] = _Sk[0][1] / -inv_det;
+		inv_Sk[1][0] = _Sk[1][0] / -inv_det;
+		inv_Sk[1][1] = _Sk[0][0] / inv_det;
+		_K[0] = _p * inv_Sk[0][0] + _p * inv_Sk[1][0];
+		_K[1] = _p * inv_Sk[0][1] + _p * inv_Sk[1][1];
+	}
+	else{
+		double inv_Sk = 1 / _Sk[0][0];
+		_K[0] = _p * inv_Sk;
+	}
+}
+

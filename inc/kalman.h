fe13557f2b5cfc8c6d6f5d3f4acb29b9880f4021
Harrison Ng <tplawyer@gmail.com>
2015-04-16 21:19:47 +0800

    Remove old kalman

--- inc/kalman.h
+++ inc/kalman.h
@@ -1,21 +1,4 @@
 /*
-typedef struct kalman_filter KF;
-
-#ifndef __KALMAN_H
-#define __KALMAN_H
-
-#include "math.h"
-
-struct kalman_filter{
-	float Q, R[2], K[2], X, x, P, p, yk[2], Sk[2][2];
-};
-
-void kalman_filter_init(KF *kf, float Q, float* R, float X, float P);
-void kalman_filtering(KF *kf, float* output, float *data1, float *data2, int length);
-
-#endif
- */
-/*
  * Kalman.h
  *
  *  Created on: 2014¦~8¤ë24¤é
